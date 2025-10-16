bde904429aa1e2fe72a81eb9c4bc429d72b41834
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-10-16 12:42:19 +0330

    make database table and migrations

--- cmd/migrate/migrations/000002_create_posts.down.sql
+++ cmd/migrate/migrations/000002_create_posts.down.sql
@@ -1,9 +1,1 @@
-CREATE TABLE IF NOT EXISTS posts (
-    id bigserial PRIMARY KEY,
-    title varchar(255) NOT NULL,
-    content text NOT NULL,
-    userid bigint NOT NULL REFERENCES users(id) ON DELETE CASCADE,
-    tags text[] DEFAULT '{}',
-    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
-    updated_at timestamp(0) with time zone NOT NULL DEFAULT NOW()
-    );
+DROP TABLE IF EXISTS posts;
