c92416e6953327f669ae1585a7c0020f197a3d2c
Har2yQn78 <hamidreza.amiri800@gmail.com>
2025-10-18 12:07:54 +0330

    password hashing

--- go.mod
+++ go.mod
@@ -2,12 +2,13 @@ module github.com/Har2yQn78/social_back.git
 
 go 1.25.1
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/lib/pq v1.10.9
+	go.uber.org/zap v1.27.0
+)
 
 require (
-	github.com/go-chi/chi/v5 v5.2.3 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	github.com/pelletier/go-toml v1.9.5 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
-	go.uber.org/zap v1.27.0 // indirect
 )
--- internal/store/users.go
+++ internal/store/users.go
@@ -3,13 +3,46 @@ package store
 import (
 	"context"
 	"database/sql"
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
 )
+
+
+var (
+	ErrDuplicateEmail    = errors.New("a user with that email already exists")
+	ErrDuplicateUsername = errors.New("a user with that username already exists")
+)
+
+type password struct {
+	text *string
+	hash []byte
+}
+
+// The Set method takes a plain-text password, hashes it with bcrypt,
+// and stores both the original and the hash in the struct.
+func (p *password) Set(text string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(text), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	p.text = &text
+	p.hash = hash
+	
+	return nil
+}
+
+// The Compare method (which we will use in the login step) checks if a given
+// plain-text password matches the stored hash.
+func (p *password) Compare(text string) error {
+	return bcrypt.CompareHashAndPassword(p.hash, []byte(text))
+}
 
 type User struct {
 	ID        int64  `json:"id"`
 	Username  string `json:"username"`
 	Email     string `json:"email"`
-	Password  string `json:"-"`
+	Password  password `json:"-"`
 	CreatedAt string `json:"created_at"`
 }
 
@@ -28,15 +61,24 @@ func (s *UsersStore) Create(ctx context.Context, user *User) error {
 		ctx,
 		query,
 		user.Username,
-		user.Password,
+		user.Password.hash, //use the hash
 		user.Email,
 	).Scan(
 		&user.ID,
 		&user.CreatedAt,
 	)
 	if err != nil {
-		return err
-	}
+			// This is how we check for specific PostgreSQL error codes.
+			// "23505" is the code for a "unique_violation".
+			switch {
+			case err.Error() == `pq: duplicate key value violates unique constraint "users_email_key"`:
+				return ErrDuplicateEmail
+			case err.Error() == `pq: duplicate key value violates unique constraint "users_username_key"`:
+				return ErrDuplicateUsername
+			default:
+				return err
+			}
+		}
 
 	return nil
 }
